ffbf97df92643a94db4193fc3aec998fbaea7b70
Sahil Sharma <sahilsharma901590@gmail.com>
2026-08-10 15:27:18 +0530

    update file

--- Backend/legal-mitra-backend/app/services/rag_service.py
+++ Backend/legal-mitra-backend/app/services/rag_service.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import re
-
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
